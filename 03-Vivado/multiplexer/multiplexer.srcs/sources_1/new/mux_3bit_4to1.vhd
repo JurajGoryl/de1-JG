2a9283741aec1410907bbff05f0b241c9f7e938e
Goryl Juraj (240913) <240913@vutbr.cz>
2023-03-01 15:11:22 +0100

    uprava

--- 03-Vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
+++ 03-Vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -32,12 +32,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity mux_3bit_4to1 is
---  Port ( );
+port (
+        b_i : in std_logic_vector(2 downto 0);
+        a_i : in std_logic_vector(2 downto 0);
+        c_i : in std_logic_vector (2 downto 0);
+        d_i : in std_logic_vector (2 downto 0);
+        sel_i : in std_logic_vector (1 downto 0);
+        f_o : out std_logic_vector (2 downto 0)
+        );
+
 end mux_3bit_4to1;
 
 architecture Behavioral of mux_3bit_4to1 is
 
 begin
 
+with sel_i select
 
+f_o <= a_i when "00",
+       b_i when "01",
+       c_i when "10",
+       d_i when others;               
+       
 end Behavioral;
